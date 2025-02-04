733a650e6525651720dbece802e3acd9dbf76b80
Dhanuka Gunarathna <nuwan.dhanuka@gmail.com>
2025-02-04 16:11:38 +0100

    initial commit

---
+++ src/partials/MetaTags.tsx
@@ -0,0 +1,11 @@
+const MetaTags = () => {
+    return(
+        <>
+            <title>Everglow CRM</title>
+            <link rel='canonical' href={ window.location.href } />
+            <meta name='description' content="Everglow CRM" />
+        </>
+    )
+}
+
+export default MetaTags;
